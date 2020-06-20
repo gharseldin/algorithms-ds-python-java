cb6661891494478d56c373b09e0e096b665ef2eb
gharseldin <gharseldin@aucegypt.edu>
2020-06-19 22:11:00 -0700

    Added Python Recursive solution

--- sorting/SelectionSort.py
+++ sorting/SelectionSort.py
@@ -10,9 +10,24 @@ def selectionSort(lst):
             # Changing this to <= will still work 
             # but it will be unstable sort
             if lst[j]<lst[min]:
-                min = j;
+                min = j
         swap(lst, i, min) 
 
+# Recursive solution
+def recursiveSelectionSort(n, lst):
+    if n == len(lst):
+        return
+    min = n
+    for j in range(n+1, len(lst)):
+        if lst[j]<lst[min]:
+            min = j
+    swap(lst, n, min)
+    recursiveSelectionSort(n+1, lst)
+
+
 test = [1,3,5,7,9,2,4,6,8,10,0,12,111,125,333,44]
+recTest = [1,3,5,7,9,2,4,6,8,10,0,12,111,125,333,44]
 selectionSort(test)
+recursiveSelectionSort(0, recTest)
 print(test)
+print(recTest)
